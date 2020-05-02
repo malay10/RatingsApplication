f3b34a73b155c4a1b12bbcd5e08c1be073b248c2
malay10 <malaybhakta10@gmail.com>
2020-05-01 19:58:40 -0500

    added filter option and minor changes

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ const ratings = {
 //total stars 
 const maxStars = 5;
 
+//summary display utility
 const updateRating = (product) => {
     //Get percentage 
     const starPercentage = (ratings[product] / 5) * 100;
@@ -32,7 +33,47 @@ const getRatings = () => {
     }
 }
 
-//Run get ratings when DOM loads;
+//tempelate for review class
+class reviewTemplate {
+    constructor(product, brand, rating, comments) {
+        this.product = product;
+        this.brand = brand;
+        this.rating = rating;
+        this.comments = comments;
+    }
+}
+let storeReview = [];
+
+//Display Ratings, add DOM content into HTML
+const createRatingDisplay = (reviewInstance) => {
+
+    //Make necessary elements to store.
+    const div = document.createElement('div');
+    const h4 = document.createElement('h4');
+    const innerDiv = document.createElement('div');
+    const stars = document.createElement('p');
+    let reviewContent = document.createElement('p');
+
+    //Update the properties
+    div.classList.add('sample-review');
+    div.classList.add(`${reviewInstance.brand}`);
+    div.classList.add('mb-10');
+    innerDiv.classList.add('content');
+    h4.appendChild(document.createTextNode(reviewInstance.product));
+    stars.textContent = reviewInstance.rating;
+    reviewContent.textContent = reviewInstance.comments;
+
+    //Display the review
+    div.appendChild(h4);
+    innerDiv.appendChild(stars);
+    innerDiv.appendChild(reviewContent);
+    div.appendChild(innerDiv);
+    displayReviews.appendChild(div);
+
+    //Is there a way to reduce DOM manipulation...?
+}
+
+//get ratings when DOM loads;
 document.addEventListener('DOMContentLoaded', getRatings);
 
 //Form Select
@@ -54,6 +95,7 @@ productSelect.addEventListener('change', (e) => {
     ratingControl.value = ratings[product];
 });
 
+//TODO: Change event type to reflect update
 //Rating control change
 ratingControl.addEventListener('blur', (e) => {
     const rating = e.target.value;
@@ -67,55 +109,42 @@ ratingControl.addEventListener('blur', (e) => {
 
     //Change the rating 
     ratings[product] = ratingControl.value;
-    // console.log(ratings[product]);
+
     updateRating(product);
-    // console.log(ratingControl.value);
     let starValue = (Math.round(ratingControl.value * 100) / 5);
     reviewStar.style.width = `${starValue}%`;
 });
 
+//review gets submitted
 reviewForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    //Make necessary elements to store.
-    const div = document.createElement('div');
-    const h4 = document.createElement('h4');
-    const innerDiv = document.createElement('div');
-    const stars = document.createElement('p');
-    let reviewContent = document.createElement('p');
 
-    //Update the properties
-    div.classList.add('sample-review');
-    div.classList.add('mb-10');
-    innerDiv.classList.add('content');
+    //Storing 
     const productValue = productSelect.options[productSelect.selectedIndex];
-    h4.appendChild(document.createTextNode(productValue.text));
-    stars.textContent = ratings[productValue.value];
-    reviewContent.textContent = review.value;
-
-    //Display the review
-    div.appendChild(h4);
-    innerDiv.appendChild(stars);
-    innerDiv.appendChild(reviewContent);
-    div.appendChild(innerDiv);
-    displayReviews.appendChild(div);
+    storeReview.push(new reviewTemplate(productValue.text, productSelect.value, ratingControl.value, review.value));
 
     //Hide the form again
     reviewForm.style.display = "none";
     review.value = "";
+
+    //display review
+    createRatingDisplay(storeReview[storeReview.length - 1]);
 });
 
 const outerStars = document.querySelector('#review-outer-stars')
 const reviewStar = document.querySelector('#review-stars');
-console.log(reviewStar);
-const slider = document.querySelector('#slider');
 
+//set width of inner star (yellow)
 const updateStars = (starPercentageRounded) => {
     const rounded = (starPercentageRounded * 5) / 100;
+    console.log()
     ratingControl.value = rounded;
     reviewStar.style.width = `${starPercentageRounded}%`;
 }
+
+//on hover effect
 outerStars.addEventListener('mousemove', (e) => {
-    //offset was going from 0 to 90.. and 180when changed the size..
+    //offset was going from 0 to 90.. and 180when changed the size was 2x
     //console.log(e.offsetX + ' ' + e.clientX);
     const starPercentage = (e.offsetX * 5 / 180) / 5 * 100;
 
@@ -124,11 +153,39 @@ outerStars.addEventListener('mousemove', (e) => {
     // console.log(starPercentageRounded);
 
     reviewStar.style.width = `${starPercentageRounded}%`;
+
+    //on click set the stars,
+    //TODO: think about making it fixed, and not change again on hover (add class etc...)
     outerStars.addEventListener('click', (e) => {
         updateStars(starPercentageRounded);
     });
 
-    outerStars.addEventListener('mouseleave', () => {
-        updateStars(starPercentageRounded);
-    });
+    // outerStars.addEventListener('mouseleave', () => {
+    //     updateStars(starPercentageRounded);
+    // });
+});
+
+//Filter to display review of specific brand. 
+const filterProduct = document.querySelector('#GroupBy');
+filterProduct.addEventListener('change', (e) => {
+    console.log(e.target.value);
+    const innerReviews = document.querySelectorAll('.sample-review');
+
+    if (e.target.value === 'all') {
+        innerReviews.forEach(r => {
+            r.classList.remove('hide');
+        })
+        return;
+    } else {
+        innerReviews.forEach(filteredReview => {
+            if (filteredReview.classList.contains(`${e.target.value}`)) {
+                console.log('executed');
+                filteredReview.classList.remove('hide');
+            } else {
+                if (!filteredReview.classList.contains('hide')) {
+                    filteredReview.classList.add('hide');
+                }
+            }
+        })
+    }
 });
